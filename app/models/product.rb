59a870f6f779fabba50a557e505db2fbd73b78cc
Sumita Coleman <sumita.coleman@gmail.com>
2016-01-04 11:26:07 +0100

    Validation added and fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :title, uniqueness: true
+    # ^ and $ will flag a security warning. It is better to use fixed markers to flag the beginnen \A and the end \z
+    validates :image_url, allow_blank: true, format: {
+        with:
+        %r{\A.(gif|jpg|png)\z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+    }
 end
